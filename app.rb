62a15bec6c0ca4b71e3715539c0c6d920077568b
David DeGraw <david.degraw@gmail.com>
2017-07-23 13:47:20 -0600

    Only return latest event for each user

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ end
 get "/" do
   content_type :json
   response['Access-Control-Allow-Origin'] = '*'
-  @events = Event.order("created_at DESC")
+  @events = Event.group(:owner)
   @events.to_json
 end
 
