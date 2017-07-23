0d3352785fb6cb136946f313db057ef3654528ba
David DeGraw <david.degraw@gmail.com>
2017-07-23 11:27:23 -0600

    init frontend

---
+++ standup/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <card></card>
+  </div>
+</template>
+
+<script>
+import card from './components/card'
+
+export default {
+  name: 'app',
+  components: {
+    card
+  }
+}
+</script>
+
+<style>
+
+</style>
