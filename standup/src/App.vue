af80e0264eab631ce85d5aa490d1ac5907030c1e
David DeGraw <david.degraw@gmail.com>
2017-07-23 11:57:09 -0600

    Boilerplate http request stuff

--- standup/src/App.vue
+++ standup/src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <card></card>
+    <el-row :gutter="20">
+      <el-col :md="6" :sm="24"><card></card></el-col>
+      <el-col :md="6" :sm="24"><card></card></el-col>
+      <el-col :md="6" :sm="24"><card></card></el-col>
+      <el-col :md="6" :sm="24"><card></card></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import card from './components/card'
 
 export default {
   name: 'app',
   components: {
     card
+  },
+
+  created () {
+    console.log('hi')
+    axios.get('http://peaceful-refuge-56771.herokuapp.com/')
+    .then(
+      response => {
+        console.log(response)
+      },
+
+      error => {
+        console.log(error)
+      })
   }
 }
 </script>
 
 <style>
-
+  html {
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  }
 </style>
--- standup/src/components/card.vue
+++ standup/src/components/card.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="card">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <h2>Card name</h2>
+      <!-- <el-button style="float: right;" type="primary">Operation button</el-button> -->
+    </div>
+    <div v-for="o in 4" :key="o" class="text item">
+      {{'List item ' + o }}
+    </div>
+  </el-card>
   </div>
 </template>
 
@@ -31,23 +23,14 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h2 {
+    line-height: 36px;
+    display: inline;
+    font-weight: 400;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .box-card {
+    /*width: 480px;*/
+  }
 </style>
