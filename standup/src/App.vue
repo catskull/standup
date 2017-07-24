664c0c6084e58fd08c5c19a7b907d6c371754d00
David DeGraw <david.degraw@gmail.com>
2017-07-23 22:40:12 -0600

    Update call for events

--- standup/src/App.vue
+++ standup/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <span v-if="events">
       <el-row v-for="chunk in events" :key="chunk[0].id" :gutter="20">
-        <el-col v-for="event in chunk" :key="event.id" :md="6" :sm="24">
+        <el-col v-for="event in chunk" :key="event.id" :md="8" :sm="24">
           <card
             :name="event.owner"
             :status="event.status"
@@ -29,11 +29,11 @@ export default {
   },
 
   created () {
-    axios.get('https://peaceful-refuge-56771.herokuapp.com/')
+    axios.get('https://peaceful-refuge-56771.herokuapp.com/events')
     .then(
       response => {
         console.log(response)
-        this.events = _.chunk(response.data, 4)
+        this.events = _.chunk(response.data, 3)
       },
 
       error => {
